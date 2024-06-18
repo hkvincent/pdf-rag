bc80618b4c66b2ed66f523128496ef25394e8fb4
vincentchan <hkvincent@users.noreply.github.com>
2024-06-18 10:12:21 +0800

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ from invoke import task
 @task
 def dev(ctx):
     ctx.run(
-        "flask --app app.web run --debug --port 8000",
+        "flask --app app.web run --debug --port 8000 --host=0.0.0.0",
         pty=os.name != "nt",
         env={"APP_ENV": "development"},
     )
