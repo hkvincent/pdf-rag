04dd991e140dcef5ff881cdd1302c5503eb0264e
vincent <hkvincentchan@gmail.com>
2024-06-18 21:15:20 +0800

    Update Docker Compose configuration to include new environment variables and dependencies

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,3 +1,4 @@
+import os
 import random
 from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
@@ -5,18 +6,12 @@ from app.chat.vector_stores import retriever_map
 from app.chat.llms import llm_map
 from app.chat.memories import memory_map
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
-from app.web.api import (
-    set_conversation_components,
-    get_conversation_components
-)
+from app.web.api import set_conversation_components, get_conversation_components
 from app.chat.score import random_component_by_score
 
-def select_component(
-    component_type, component_map, chat_args
-):
-    components = get_conversation_components(
-        chat_args.conversation_id
-    )
+
+def select_component(component_type, component_map, chat_args):
+    components = get_conversation_components(chat_args.conversation_id)
     previous_component = components[component_type]
 
     if previous_component:
@@ -29,35 +24,24 @@ def select_component(
 
 
 def build_chat(chat_args: ChatArgs):
-    retriever_name, retriever = select_component(
-        "retriever",
-        retriever_map,
-        chat_args
-    )
-    llm_name, llm = select_component(
-        "llm",
-        llm_map,
-        chat_args
-    )
-    memory_name, memory = select_component(
-        "memory",
-        memory_map,
-        chat_args
-    )
+    retriever_name, retriever = select_component("retriever", retriever_map, chat_args)
+    llm_name, llm = select_component("llm", llm_map, chat_args)
+    memory_name, memory = select_component("memory", memory_map, chat_args)
     set_conversation_components(
         chat_args.conversation_id,
         llm=llm_name,
         retriever=retriever_name,
-        memory=memory_name
+        memory=memory_name,
     )
-    
 
-    condense_question_llm = ChatOpenAI(streaming=False)
+    condense_question_llm = ChatOpenAI(
+        streaming=False, openai_api_base=os.getenv("OPENAI_API_BASE_URL")
+    )
 
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
         condense_question_llm=condense_question_llm,
         memory=memory,
         retriever=retriever,
-        metadata=chat_args.metadata
+        metadata=chat_args.metadata,
     )
